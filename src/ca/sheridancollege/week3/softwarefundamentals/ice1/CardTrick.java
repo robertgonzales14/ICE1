810e8b6fed210c5ca1473a1160cc4215dd99bde3
Robth <Robth@RobbieGonzo.hitronhub.home>
2021-02-08 10:14:28 -0500

    Updated CardTrick to where you now have a lucky card which values cannot be changed, and the magicians hand is now shown after the user inputs there value and suit

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -24,17 +24,17 @@ public class CardTrick {
         Scanner input = new Scanner(System.in);
         Card[] magicHand = new Card[7];
         
+        Card luckyCard = new Card();//New Card class just for luckyCard
+        
+        luckyCard.setValue(10);//Lucky card is set to a value of 10 and cannot be changed
+        luckyCard.setSuit(Card.SUITS[2]);//Lucky card is set to Spades and cannot be changed
+        
+        System.out.println("Your Lucky Card is: " + luckyCard);//Tells the user what there lucky card is
+        
         boolean result = false;//For the result
         Random rand = new Random();//initializes Random util
         
-        for (int i=0; i<magicHand.length; i++)
-        {
-            Card c = new Card();
-            
-            c.setValue(rand.nextInt(13)+1);
-            c.setSuit(Card.SUITS[rand.nextInt(4)]);
-            magicHand[i] = c;//Sets the magicians hand to a variable
-        }
+        
 
         Card userInput = new Card();
         
@@ -48,20 +48,48 @@ public class CardTrick {
         userInput.setSuit(suit);//Sets the users input in setSuit method
         
         System.out.println("Searching for: " + userInput);
-    
+        
+        System.out.println("\nMagicians Hand: ");
+        for (int i=0; i<magicHand.length; i++)
+        {
+            Card c = new Card();
+            
+            c.setValue(rand.nextInt(13)+1);
+            c.setSuit(Card.SUITS[rand.nextInt(4)]);
+            magicHand[i] = c;//Sets the magicians hand to a variable
+            System.out.println(c);//Displays what was in the magicians after the user declares there card
+        }
+        
         //To determine if the users card is in the magicians hand
         for (int i = 0; i < magicHand.length; i++) //The loops is the length to the array
         {
             
             if ((magicHand[i].getValue() == userInput.getValue()) && magicHand[i].getSuit().equalsIgnoreCase(userInput.getSuit())) 
             {
-                System.out.println("Card is found in the magicians hand");//Tells the user the carder is found in the magicians hand
+                System.out.println("\nYour Card is found in the magicians hand");//Tells the user the carder is found in the magicians hand
                 result = true;
                 break;//Break is incluced so the result does not loop to the arrays length (7 times)
             }
             else//If the userInput does not equal the the magicians hand
             {
-                System.out.println("Card is not found in the magicians hand.");//Tells the user the card is not in the magicians hand
+                System.out.println("\nYour Card is not found in the magicians hand.");//Tells the user the card is not in the magicians hand
+                break;//Break is incluced so the result does not loop to the arrays length (7 times)
+            }
+        }
+        
+        //Lucky Card
+        for (int i = 0; i < magicHand.length; i++) //The loops is the length to the array
+        {
+            
+            if ((magicHand[i].getValue() == luckyCard.getValue()) && magicHand[i].getSuit().equalsIgnoreCase(luckyCard.getSuit())) 
+            {
+                System.out.println("\nLucky Card is found in the magicians hand");//Tells the user the carder is found in the magicians hand
+                result = true;
+                break;//Break is incluced so the result does not loop to the arrays length (7 times)
+            }
+            else//If the userInput does not equal the the magicians hand
+            {
+                System.out.println("\nLucky Card is not found in the magicians hand.");//Tells the user the card is not in the magicians hand
                 break;//Break is incluced so the result does not loop to the arrays length (7 times)
             }
         }
